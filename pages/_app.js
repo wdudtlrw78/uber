9dfeb23c41bfc74b91b53fce5b5c0b7e612c5fa6
momo <wdudtlrw78@gmail.com>
2021-12-08 00:45:15 +0900

    feat: search page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,90 @@
 import tw from 'tailwind-styled-components';
 import Map from './components/Map';
+import UserImage from 'next/image';
+import profile from '../public/profile.jpg';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <Wrapper>
       <Map />
-      <ActionItems>Start</ActionItems>
+
+      <ActionItems>
+        {/* Header */}
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+          <Profile>
+            <Name>Yeong-Sik, Mo</Name>
+            <UserImage
+              src={profile}
+              className="user-image"
+              alt="Picture of the author"
+              width={48}
+              height={48}
+            />
+          </Profile>
+        </Header>
+
+        {/* ActionButtons */}
+        <ActionButtons>
+          <Link href="/search">
+            <ActionButton>
+              <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png" />
+              Ride
+            </ActionButton>
+          </Link>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png" />
+            2-Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png" />
+            Reserve
+          </ActionButton>
+        </ActionButtons>
+        {/* InputButton */}
+        <InputButton>Where to?</InputButton>
+      </ActionItems>
     </Wrapper>
   );
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen
+  flex flex-col h-screen
 `;
 
 const ActionItems = tw.div`
-  flex-1
+  flex-1 p-4
+`;
+
+const Header = tw.div`
+  flex justify-between items-center
+`;
+
+const UberLogo = tw.img`
+  h-28
+`;
+
+const Profile = tw.div`
+  flex items-center
+`;
+
+const Name = tw.div`
+  mn-4 w-20 text-sm
+`;
+
+const ActionButtons = tw.div`
+  flex
+`;
+
+const ActionButton = tw.div`
+  flex bg-gray-200 flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl
+`;
+
+const ActionButtonImage = tw.img`
+  h-3/5
+`;
+
+const InputButton = tw.div`
+  h-20 bg-gray-200 text-2xl p-4 flex items-center rounded-lg mt-8
 `;
